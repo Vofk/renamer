01138f51606fd6395cb81f7e361b0c163d2ee026
Vofk <v.kovalenko.tv>
2022-06-18 16:53:46 +0300

    Some renames of variables

--- rnen.py
+++ rnen.py
@@ -4,9 +4,9 @@ from os.path import join, split
 from datetime import datetime
 import re
 
-legaltypes = ('VMZ', 'SNH', 'SUJ', 'CYT', 'GRF')
+legal_types = ('VMZ', 'SNH', 'SUJ', 'CYT', 'GRF')
 
-legalprojects = ('EN', 'UR', 'SU', 'VS')
+legal_projects = ('EN', 'UR', 'SU', 'VS')
 
 replacer = {'_': ' ',
             '-': '',
@@ -36,14 +36,9 @@ replacer = {'_': ' ',
             '.MP4.MP4': '.MP4',
 
             }
-#  0     1    2     3
-# U24 190322 SNH OBSTRILY AUDIO.mp4
-#  0   1      2    3
-# U24 DDMMYY TYP NAME
+
 
 # Предварительная подготовка имени файла
-
-
 def nameprep(file_path):
     new_name = os.path.basename(file_path).upper()
     new_name = re.sub(r"^\s+", "", new_name)
@@ -72,8 +67,8 @@ def getdate(file_path):
     print("atime = ", datetime.fromtimestamp(stat.st_atime).strftime("%d%m%y"))
     print("ctime = ", datetime.fromtimestamp(stat.st_ctime).strftime("%d%m%y"))
     print("mtime = ", datetime.fromtimestamp(stat.st_mtime).strftime("%d%m%y"))
-    filedate = datetime.fromtimestamp(stat.st_birthtime).strftime("%d%m%y")
-    return filedate
+    file_date = datetime.fromtimestamp(stat.st_birthtime).strftime("%d%m%y")
+    return file_date
 
 
 if __name__ == '__main__':
@@ -83,119 +78,108 @@ if __name__ == '__main__':
         base_path = '/Volumes/Macintosh HD/Users/boba/Desktop/TESTUR'
     print(f"base_path: {base_path}")
     for f in os.listdir(base_path):
-        pathfile = join(base_path, f)
+        path_file = join(base_path, f)
 
-        if os.path.isdir(pathfile):
+        if os.path.isdir(path_file):
             continue
 
         if f[0] == '.':
             continue
 
-        datefile = getdate(pathfile)                                    # храним дату создания файла
+        date_file = getdate(path_file)                                    # храним дату создания файла
 
-        print(datefile)
+        print(date_file)
         print(f'ISX NAME: {f}')
 
-        nametemp, exttemp = nameprep(join(base_path, f)).split('.')     # храним отдельно имя и расширение файла
-        delparts = []                                                   # список частей на удаление
-        newparts = ['U24', 'PROJECT', 'DATE', 'TYPE']                   # список обязательных частей
+        name_temp, ext_temp = nameprep(join(base_path, f)).split('.')     # храним отдельно имя и расширение файла
+        del_parts = []                                                   # список частей на удаление
+        new_parts = ['U24', 'PROJECT', 'DATE', 'TYPE']                   # список обязательных частей
 
-        if re.match(r'V\s\d\d\s', nametemp):                            # ищем файлы начинающиеся с V [0-9][0-9]
-            nametemp = re.sub(r'V\s\d\d\s', '', nametemp)
-            newparts[1] = 'VS'
+        if re.match(r'V\s\d\d\s', name_temp):                            # ищем файлы начинающиеся с V [0-9][0-9]
+            name_temp = re.sub(r'V\s\d\d\s', '', name_temp)
+            new_parts[1] = 'VS'
 
-        nameparts = nametemp.split(' ')                                 # делим имя на части
+        name_parts = name_temp.split(' ')                                 # делим имя на части
 
-        if nameparts[1] in legalprojects:                               # проверяем второй блок (код проекта)
-            newparts[1] = nameparts[1]
-            delparts.append(nameparts[1])
+        if name_parts[1] in legal_projects:                               # проверяем второй блок (код проекта)
+            new_parts[1] = name_parts[1]
+            del_parts.append(name_parts[1])
 
-        if re.match(r'\b\d\d\b', nameparts[0]):                             # ищем файлы начинающиеся с "[0-9][0-9]"
-            newparts[1] = 'UR'
-            delparts.append(nameparts[0])
+        if re.match(r'\b\d\d\b', name_parts[0]):                             # ищем файлы начинающиеся с "[0-9][0-9]"
+            new_parts[1] = 'UR'
+            del_parts.append(name_parts[0])
 
-        if re.match(r'\bV\d\d\b', nameparts[0]):                            # ищем файлы начинающиеся с "V[0-9][0-9]"
-            newparts[1] = 'VS'
-            delparts.append(nameparts[0])
+        if re.match(r'\bV\d\d\b', name_parts[0]):                            # ищем файлы начинающиеся с "V[0-9][0-9]"
+            new_parts[1] = 'VS'
+            del_parts.append(name_parts[0])
 
-        for part in nameparts:
+        for part in name_parts:
 
             if part == 'U24':
-                delparts.append(part)
+                del_parts.append(part)
                 continue
 
-            # if re.match(r'\b\d{2}\b', part):                          # две цифры могут означать порядковый номер
-            #     newparts[1] = 'UR'
-            #     delparts.append(part)
-            #     print('find number',part, delparts)
-
-            # if re.match(r'\bV\d{2}\b', part):                         # V и две цифры могут означать порядковый номер
-            #     newparts[1] = 'VS'
-            #     delparts.append(part)
-            #     print('find number', part, delparts)
-
             if re.match(r'\bWSH\b', part):                              # вариант обозначения военного щоденника
-                newparts[1] = 'VS'
-                delparts.append(part)
+                new_parts[1] = 'VS'
+                del_parts.append(part)
                 continue
 
             if re.match(r'\d\d\d\d\d\d', part):                         # поиск возможной даты
                 print('part = ',
-                      part if datetime.strptime(part, "%d%m%y") < datetime.strptime(datefile, "%d%m%y") else datefile)
-                if newparts[2] == 'DATE':
-                    if datetime.strptime(part, "%d%m%y") < datetime.strptime(datefile, "%d%m%y"):
-                        newparts[2] = part
+                      part if datetime.strptime(part, "%d%m%y") < datetime.strptime(date_file, "%d%m%y") else date_file)
+                if new_parts[2] == 'DATE':
+                    if datetime.strptime(part, "%d%m%y") < datetime.strptime(date_file, "%d%m%y"):
+                        new_parts[2] = part
                     else:
-                        newparts[2] = datefile
-                delparts.append(part)
+                        new_parts[2] = date_file
+                del_parts.append(part)
                 continue
 
-            if len(part) == 3 and part in legaltypes:                   # поиск возможного типа
-                if newparts[3] == 'TYPE':
-                    newparts[3] = part
-                delparts.append(part)
+            if len(part) == 3 and part in legal_types:                   # поиск возможного типа
+                if new_parts[3] == 'TYPE':
+                    new_parts[3] = part
+                del_parts.append(part)
                 continue
 
-        if newparts[2] == 'DATE':                                       # дата в имени не найдена берем из свойств файла
-            newparts[2] = datefile
+        if new_parts[2] == 'DATE':                                       # дата в имени не найдена берем из свойств файла
+            new_parts[2] = date_file
 
-        if newparts[3] == 'TYPE':                                     # стандартный тип в имени не найден, ищем варианты
-            if re.search(r'\bVMZ', nametemp):
-                newparts[3] = 'VMZ'
-            if re.search(r'\bOTRAG', nametemp):
-                newparts[3] = 'VMZ'
-            if re.search(r'\bOTRAZHONKA', nametemp):
-                newparts[3] = 'VMZ'
-            if re.search(r'\bDAIDJEST', nametemp):
-                newparts[3] = 'SUJ'
-            if re.search(r'\bDAIDZHEST', nametemp):
-                newparts[3] = 'SUJ'
-            if re.search(r'\bDIDGEST', nametemp):
-                newparts[3] = 'SUJ'
-            if re.search(r'\bCYTATA', nametemp):
-                newparts[3] = 'SNH'
+        if new_parts[3] == 'TYPE':                                     # стандартный тип в имени не найден, ищем варианты
+            if re.search(r'\bVMZ', name_temp):
+                new_parts[3] = 'VMZ'
+            if re.search(r'\bOTRAG', name_temp):
+                new_parts[3] = 'VMZ'
+            if re.search(r'\bOTRAZHONKA', name_temp):
+                new_parts[3] = 'VMZ'
+            if re.search(r'\bDAIDJEST', name_temp):
+                new_parts[3] = 'SUJ'
+            if re.search(r'\bDAIDZHEST', name_temp):
+                new_parts[3] = 'SUJ'
+            if re.search(r'\bDIDGEST', name_temp):
+                new_parts[3] = 'SUJ'
+            if re.search(r'\bCYTATA', name_temp):
+                new_parts[3] = 'SNH'
 
-        if newparts[3] == 'TYPE':                                       # тип не найден
-            newparts.remove('TYPE')
+        if new_parts[3] == 'TYPE':                                       # тип не найден
+            new_parts.remove('TYPE')
 
-        if newparts[1] == 'PROJECT':                                    # код проекта не установлен
-            newparts.remove('PROJECT')
-            # newparts[1] = 'UR'
+        if new_parts[1] == 'PROJECT':                                    # код проекта не установлен
+            new_parts.remove('PROJECT')
 
-        print(f'delparts : {delparts}')
-        print(f'nameparts: {nameparts}')
+        print(f'delparts : {del_parts}')
+        print(f'nameparts: {name_parts}')
 
-        for part in delparts:                                           # удаляем из списка частей обработанные
-            nameparts.remove(part)
+        for part in del_parts:                                           # удаляем из списка частей обработанные
+            name_parts.remove(part)
 
-        newparts += nameparts                                           # к шаблонной части добавляем специфическую
-        newname = ''
+        new_parts += name_parts                                           # к шаблонной части добавляем специфическую
+        new_name = ''
 
-        for x in range(len(newparts)):                                  # собираем новое имя
-            if x == len(newparts) - 1:
-                newname += newparts[x] + '.'
+        for x in range(len(new_parts)):                                  # собираем новое имя
+            if x == len(new_parts) - 1:
+                new_name += new_parts[x] + '.'
             else:
-                newname += newparts[x] + ' '
-        newname += exttemp                                              # возвращаем расширение файла
-        print(f"NEW NAME: {newname}\n")
-        os.rename(pathfile, join(split(pathfile)[0], newname))          # переименовываем
+                new_name += new_parts[x] + ' '
+        new_name += ext_temp                                              # возвращаем расширение файла
+        print(f"NEW NAME: {new_name}\n")
+        os.rename(path_file, join(split(path_file)[0], new_name))          # переименовываем
